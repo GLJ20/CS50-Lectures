66ef3bf49cd3c2b3ac47314ff3b1e88a5d0bd97c
Malak <malakalmahari7042@gmail.com>
2025-08-19 19:31:59 +0300

    used cs50 solution since it is better

--- lect-1/scrabble.c
+++ lect-1/scrabble.c
@@ -1,63 +1,60 @@
-#include <ctype.h>
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
+// #include <ctype.h>
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
 
-// Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+// // Points assigned to each letter of the alphabet
+// int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+// int compute_score(string word);
 
-int main(void)
-{
-    // Prompt the user for two words
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
-    
-    for(int i = 0; word1[i]; i++)
-    {
-	word1[i] = tolower(word1[i]);
-    }
+// int main(void)
+// {
+//     // Prompt the user for two words
+//     string word1 = get_string("Player 1: ");
+//     string word2 = get_string("Player 2: ");
 
-    for(int i = 0; word1[i]; i++)
-    {
-	word1[i] = tolower(word1[i]);
-    }
+//     // Compute the score of each word
+//     int score1 = compute_score(word1);
+//     int score2 = compute_score(word2);
 
-    // Compute the score of each word
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+//     // Print the winner
+//     if (score1 > score2)
+//     {
+//         printf("Player 1 wins!\n");
+//     }
+//     else if (score1 < score2)
+//     {
+//         printf("Player 2 wins!\n");
+//     }
+//     else
+//     {
+//         printf("Tie!\n");
+//     }
+// }
 
-    // Print the winner   
-    if (score1 > score2)
-    {
-        printf("Player 1 wins!\n");
-    }
-    else if (score1 < score2)
-    {
-        printf("Player 2 wins!\n");
-    }
-    else
-    {
-        printf("Tie!\n");
-    }
-}
+// int compute_score(string word)
+// {
+//     // Keep track of score
+//     int score = 0;
+//     int len = strlen(word);
 
-int compute_score(string word)
-{
-    // Keep track of score
-    int score = 0;
-    len = strlen(word)
+//     // Compute score for each character
+//     for (int i = 0; i < len; i++)
+//     {
+//         //word[i] will capture each letter so word[0] will be c in code. and it will be
+// 	    // 'c' so the char, and we minus it from char a so 99 - 97 = 3 and index 3 in
+// 	    // points is 3 then it loops again right? so it does same for each char and we
+// 	    // gte the score
+//         if (isupper(word[i]))
+//         {
+//             score += POINTS[word[i] - 'A'];
+//         }
+//         else if (islower(word[i]))
+//         {
+//             score += POINTS[word[i] - 'a'];
+//         }
+//     }
 
-    // Compute score for each character
-    for (int i = 0; i < len; i++)
-    {	  
-        //word[i] will capture each letter so word[0] will be c in code. and it will be
-	  // 'c' so the char, and we minus it from char a so 99 - 97 = 3 and index 3 in 
-	  // points is 3 then it loops again right? so it does same for each char and we 
-	  // gte the score
-        score += POINTS[word[i] - 'a'];
-    }
-
-    return score;
-}
+//     return score;
+// }
