011730bb04284a905c3038bc9a8e86d48a83b7f6
Malak <malakalmahari7042@gmail.com>
2025-08-21 22:57:18 +0300

    greet c is a file to showcase how to accept command line args

---
+++ lect-2/lect/greet.c
@@ -0,0 +1,12 @@
+// #include <cs50.h>
+// #include <stdio.h>
+// // first argument passed is count of args then argv is vector which is another name for array
+// //so the collection of args
+// //argv[0] is considered as the name of the file so ./greet
+// int main(int argc, string argv[])
+// {
+//     for(int i = 0; i < argc; i++)
+//     {
+//         printf("%s\n", argv[i]);
+//     }
+// }
