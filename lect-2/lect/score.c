cade0d5dfe59011508f072c210db298e7065f95a
GLJ20 <malakalmahari7042@gmail.com>
2025-08-21 09:08:38 +0300

    a bit better but still poor design

--- lect-2/lect/score.c
+++ lect-2/lect/score.c
@@ -1,12 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 3;
+
 int main(void)
 {
-  int scores[3];
-  scores[0] = 72;
-  scores[1] = 73;
-  scores[2] = 33;
+  int scores[N];
+  for(int i = 0; i < N; i++)
+    {
+      scores[i] = get_int("Score: ");
+    }
 
   printf("Average: %f\n", (scores[0] + score[1] + scores[2]) / 3.0);
 }
+
