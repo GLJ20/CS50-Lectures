51b09609ef82d59939109c6a5041f305152d7a9f
Malak <malakalmahari7042@gmail.com>
2025-08-21 22:41:29 +0300

    how to convert lower case to uppercase without toUpper

---
+++ lect-2/lect/uppercase.c
@@ -0,0 +1,23 @@
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(void)
+// {
+//     string s = get_string("Before: ");
+//     printf("After: ");
+
+//     for (int i = 0; n = strlen(s); i < n; i++)
+//     {
+//         if(s[i] >= 'a' && s[i] <= 'z')
+//         {
+//             //cause literally the difference between the ASCII is 32 so a which is 97, 97 - 32 = 65
+//             printf("%c", s[i] - 32);
+//         }
+//         else
+//         {
+//             printf("%c", s[i]);
+//         }
+//     }
+//     printf
+// }
