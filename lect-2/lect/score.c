fe0ea2fd2dd127099d19c1eb1ad5463f61067ea1
GLJ20 <malakalmahari7042@gmail.com>
2025-08-21 09:21:33 +0300

    better design

--- lect-2/lect/score.c
+++ lect-2/lect/score.c
@@ -1,16 +1,28 @@
-#include <cs50.h>
-#include <stdio.h>
+// #include <cs50.h>
+// #include <stdio.h>
 
-const int N = 3;
+// const int N = 3;
 
-int main(void)
-{
-  int scores[N];
-  for(int i = 0; i < N; i++)
-    {
-      scores[i] = get_int("Score: ");
-    }
+// float average(int N, int array[]);
 
-  printf("Average: %f\n", (scores[0] + score[1] + scores[2]) / 3.0);
-}
+// int main(void)
+// {
+//   //cant define an empty array like JS, array = []
+//   int scores[N];
+//   for(int i = 0; i < N; i++)
+//     {
+//       scores[i] = get_int("Score: ");
+//     }
 
+//   printf("Average: %f\n", average(N, scores);
+// }
+// //whne passing an array into a function in C we need to write [] unlike JS
+// float average(int length, int array[])
+// {
+//   int sum = 0;
+//   for(i = 0; i < length; i++)
+//   {
+//     sum += array[i];
+//   }
+//   return sum / (float) length;
+// }
