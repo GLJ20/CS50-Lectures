8b969ac6b18ad3a74b29c533e5eab779022d3f8c
GLJ20 <malakalmahari7042@gmail.com>
2025-08-21 09:53:24 +0300

    a function to check length of a string!!

---
+++ lect-2/lect/length.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  string name = get_string("Name: ");
+
+  int n = 0;
+  //check if the char is not nul
+  while(name[n] != '\0')
+  {
+    n++;
+  }
+  printf("%i\n", n);
+}
