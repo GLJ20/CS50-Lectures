5e0e177cc2cf8dc9599c3db8abb381113c3a13e0
GLJ20 <malakalmahari7042@gmail.com>
2025-09-02 09:34:52 +0300

    how to actually copy in C

--- copy.c
+++ copy.c
@@ -1,16 +1,35 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
   //if we write hI, we want s to be hI but t to be HI
-  string s = get_string("s: ");
-  string t = s;
+  char *s = get_string("s: ");
+  //first make var t get same memeory as s by using malloc and strlen + 1 to handle correct bytes of
+  //s and add 1 to it to have value \0
+  char *t = malloc(strlen(s) + 1);
 
-  make the first letter uppercase
-  t[0] = toupper(t[0]);
+  if(t == NULL)
+  {
+    return 1; //if t has no memory dont even try to do the for loop
+  }
+  for (int i = 0, n = strlen(s); i <= n; i++)
+  {
+    //copy each char in s to t so s[0] = h is value of t[0] and so on
+    t[i] = s[i];
+  }
+
+  //not a good idea to try to capitalize zero, and this zero can come because the user by mistake
+  //clicks on enter without writing anything so we need to handle that
+  if(strlen(t) > 0)
+  {
+     //make the first letter uppercase
+     t[0] = toupper(t[0]);
+  }
+ 
 
   //However, this will print HI for both because string s is nothing but char *s so a pointer
   //and this s is taking 8 bytes in the memory and it has the address of the char h in another
@@ -21,3 +40,4 @@ int main(void)
   printf("s: %s\n", s);
   printf("t: %s\n", t);
 }
+//if something went wrong with malloc it will return null, not nul like \0 no null so 0
